9d261a32f0953f071c6708a3470d4ecf48730060
Landon Craft <133186492+LandoTheDeveloper@users.noreply.github.com>
2025-02-04 20:32:27 -0500

    Update maximum-ascending-subarray-sum.java

--- algorithms/java/src/maximum-ascending-subarray-sum.java
+++ algorithms/java/src/maximum-ascending-subarray-sum.java
@@ -3,7 +3,7 @@ Given an array of positive integers nums, return the maximum possible sum of an 
 
 A subarray is defined as a contiguous sequence of numbers in an array.
 
-A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
+A subarray [numsl, nums[l+1], ..., nums[r-1], nums[r] is ascending if for all i where l <= i < r, nums[i]  < nums[i+1]. Note that a subarray of size 1 is ascending.
 */
 
 class Solution {
